7cbf3a0356e795f169865e5633f92bb0af3de5fa
Jianzu Guan <guan-1995@hotmail.com>
2017-04-25 20:29:19 +0100

    add player inventory UI

---
+++ Assets/_Scripts/InventoryItemUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemUI : MonoBehaviour {
+
+    public Image icon;
+    public Text itemName;
+    public Text itemCount;
+
+    public void Setup(Sprite icon, string itemName, int itemCount, float y) {
+        this.icon.sprite = icon;
+        this.itemName.text = itemName;
+        this.itemCount.text = "X " + itemCount;
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(0, y);
+    }
+}
