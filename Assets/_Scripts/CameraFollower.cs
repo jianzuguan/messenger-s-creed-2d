bcb0574a153657dc52256752ff3659b09483f6c3
Jianzu Guan <guan-1995@hotmail.com>
2017-04-25 13:21:55 +0100

    added all in one scene
    added zero friction to player character
    added function to switch levels

--- Assets/_Scripts/CameraFollower.cs
+++ Assets/_Scripts/CameraFollower.cs
@@ -5,29 +5,37 @@ public class CameraFollower : MonoBehaviour {
 
     public GameObject player;
     public bool canGoBack;
-    
+
+    public GameObject farWest;
     public GameObject farEast;
 
-    float xMin;
-    float xMax = float.MaxValue;
+    public float xMin;
+    public float xMax;
 
     Vector3 offset;
     float x, y, z;
+
+    Camera cam;
+    public float camHeight;
+    public float camWidth;
 
     void Start() {
         if (player == null)
             player = GameObject.FindWithTag("Player");
         //offset = transform.position - player.transform.position;
 
-        Camera cam = Camera.main;
-        float camHeight = 2f * cam.orthographicSize;
-        float camWidth = camHeight * cam.aspect;
+        cam = Camera.main;
+        cam.aspect = 16f / 9f;
 
-        Camera.main.aspect = 16f / 9f;
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
 
-        xMin = transform.position.x;
-        if (farEast != null)
+        if (farWest != null) {
+            xMin = farWest.transform.position.x + camWidth / 2 + 0.5f;
+        }
+        if (farEast != null) {
             xMax = farEast.transform.position.x - camWidth / 2 - 0.5f;
+        }
 
         x = transform.position.x;
         y = transform.position.y;
@@ -43,5 +51,22 @@ public class CameraFollower : MonoBehaviour {
             x = player.transform.position.x + 5;
             transform.position = new Vector3(x, y, z);
         }
+    }
+
+    public void RefreshBoundary(GameObject farWest, GameObject farEast, Transform cameraPoint) {
+        x = cameraPoint.position.x;
+        y = cameraPoint.position.y;
+        z = cameraPoint.position.z;
+
+        this.farWest = farWest;
+        this.farEast = farEast;
+
+        if (farWest != null) {
+            xMin = farWest.transform.position.x + camWidth / 2 + 0.5f;
+        }
+        if (farEast != null) {
+            xMax = farEast.transform.position.x - camWidth / 2 - 0.5f;
+        }
+        
     }
 }
--- Assets/_Scripts/LevelEntrance.cs
+++ Assets/_Scripts/LevelEntrance.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelEntrance : MonoBehaviour {
+    
+    public Camera camera;
+    public GameObject player;
+    public GameObject cover;
+    public GameObject farWest;
+    public GameObject farEast;
+    public Transform cameraPoint;
+    public Transform playerPoint;
 
-    public string scenePath;
+    // Use this for initialization
+    void Start() {
+        if (camera == null) {
+            camera = Camera.main;
+        }
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") {
-            SceneManager.LoadScene(scenePath);
+            camera.GetComponent<CameraFollower>().RefreshBoundary(farWest, farEast, cameraPoint);
+
+            camera.transform.position = cameraPoint.position;
+            player.transform.position = playerPoint.position;
+
         }
     }
 
