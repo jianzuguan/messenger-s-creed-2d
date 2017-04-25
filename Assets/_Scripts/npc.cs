342ed2424fd4d8b44af3df31e6611a0deb45ef51
Ztz <ztz_1@qq.com>
2017-04-25 18:42:17 +0100

    panel modify + item preafb + UID NPC scripts

--- Assets/_Scripts/npc.cs
+++ Assets/_Scripts/npc.cs
@@ -9,37 +9,42 @@ public class npc : MonoBehaviour {
 	public int indx;
 	public Text txt;
 	public Text name_txt;
-	public GameObject panel;
-
+	private GameObject panel;
+	public bool iSActivated;
+	public Button bt;
 	// Use this for initialization
 	void Start () {
+		iSActivated = false;
 		//txt = GameObject.FindGameObjectWithTag ("conv_holder").guiText;
 		//name_txt = GameObject.FindGameObjectWithTag ("npc_name").guiText;
 		indx=0;
-		//panel = GameObject.FindGameObjectWithTag ("panel3");
+		panel = GameObject.FindGameObjectWithTag ("panel3");
+		Button btn = bt.GetComponent<Button>();
+		btn.onClick.AddListener(npc_next_dia);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.P)&& panel.activeSelf) {
+		if (Input.GetKeyUp (KeyCode.P)&& panel.activeSelf&&iSActivated) {
 			npc_next_dia ();
 		}
 		if (indx > convs.Count - 1) {
-			panel.SetActive (false);
-			indx = 0;
-		
+			//panel.SetActive (false);
+			//indx = 0;
+			//iSActivated = false;
+			throw_panel();
 		}
 	}
 
 
 	private void npc_next_dia(){
-		//indx++;
-		Debug.Log ("next_dia");
+		indx++;
+		//Debug.Log ("next_dia");
 		Debug.Log (convs[indx]);
 		if (indx <= convs.Count-1) {
 			txt.text = convs [indx];
 			name_txt.text = npc_name;
-			indx++;
+			//indx++;
 			//Debug.Log (convs.Count);
 		} else {
 			//Debug.Log("set done");
@@ -49,17 +54,26 @@ public class npc : MonoBehaviour {
 
 	}
 	public void OnTriggerEnter2D(Collider2D cl){
-		if (cl.gameObject.tag=="Player")
-		//throw_panel ();
-		panel.SetActive (true);
-		indx = 0;
-		txt.text = convs [indx];
-		name_txt.text = npc_name;
-	}
+		if (cl.gameObject.tag == "Player")
+		{		//throw_panel ();
+			iSActivated = true;
 
+			panel.SetActive (true);
+			indx = 0;
+			txt.text = convs [indx];
+			name_txt.text = npc_name;
+		}
+	}
+	public void OnTriggerExit2D(Collider2D cl){
+		if (cl.gameObject.tag == "Player") {
+			throw_panel();
+		}
+	}
 	public void throw_panel(){
 		panel.SetActive (false);
 		indx = 0;
+		iSActivated = false;
+
 		//Debug.Log("set done");
 		//name_txt.text = npc_name;
 		//txt.text = convs [indx];
--- Assets/_Scripts/uid.cs
+++ Assets/_Scripts/uid.cs
@@ -5,17 +5,37 @@ using UnityEngine.UI;
 
 public class uid : MonoBehaviour {
 	public Dictionary<string,int> uids = new Dictionary<string,int> ();
+	public List<Item> backpack = new List<Item>();
 	public Text uid_txt;
 	private string otpt;
+	public GameObject wine1;
+	//public Transform parenttrans;
+	public RectTransform parenttrans;
+	//public ini_test(){
+
+	//}
 	// Use this for initialization
 	void Start () {
 		//Dictionary<string,int> uids = new Dictionary<string,int> ();
 		uids.Add("Gold:",1000);
 		uids.Add ("Beer:", 1);
 		uids.Add ("Names", 0);
-		UpdateUID ();
+		//UpdateUID ();
+		ini_test ();
 	}
-	
+	public void ini_test()
+	{
+
+		GameObject ret=Instantiate (wine1, parenttrans,false);
+		Vector3 offset = new Vector3(0.0f , 50f, 0f);
+		//Transform newtrans =new Vector3 (parenttrans.position.x, parenttrans.position.y - 50, parenttrans.position.z);
+		parenttrans.localPosition-=offset;
+		GameObject ret2=Instantiate (wine1,parenttrans);
+		//ret2.transform.SetParent (ret.transform, false);
+
+
+		//Instantiate (wine1,parenttrans);
+	}
 	// Update is called once per frame
 	public void UpdateUID () {
 		otpt="";
@@ -26,4 +46,11 @@ public class uid : MonoBehaviour {
 		uid_txt.text = otpt;
 		//Debug.Log(uids["Gold:"]);
 	}
+
+}
+[System.Serializable]
+public class Item{
+	public string item_name;
+	public int item_number;
+	public string item_sprite;
 }
