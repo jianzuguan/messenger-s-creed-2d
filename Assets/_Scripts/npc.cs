6529847ac22a678afc4eaea5f48f8042268aceef
Ztz <ztz_1@qq.com>
2017-04-25 20:31:05 +0100

    npc script

--- Assets/_Scripts/npc.cs
+++ Assets/_Scripts/npc.cs
@@ -6,62 +6,90 @@ using UnityEngine.UI;
 public class npc : MonoBehaviour {
 	public string npc_name;
 	public List<string> convs;
-	public int indx;
+	public List<string> convs1;
+	public List<string> convs2;
+	private int indx;
 	public Text txt;
 	public Text name_txt;
-	private GameObject panel;
+	public GameObject panel;
 	public bool iSActivated;
-	public Button bt;
+	public int current_state;
+	public bool mission_comp;
 	// Use this for initialization
 	void Start () {
-		iSActivated = false;
-		//txt = GameObject.FindGameObjectWithTag ("conv_holder").guiText;
-		//name_txt = GameObject.FindGameObjectWithTag ("npc_name").guiText;
-		indx=0;
-		panel = GameObject.FindGameObjectWithTag ("panel3");
-		Button btn = bt.GetComponent<Button>();
-		btn.onClick.AddListener(npc_next_dia);
+		current_state = 0;
 	}
-
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.P)&& panel.activeSelf&&iSActivated) {
-			npc_next_dia ();
+		if (Input.GetKeyUp (KeyCode.P) && panel.activeSelf && iSActivated) {
+			if (mission_comp&&current_state!=2) {
+				current_state = 1;
+			}
+			npc_next_dia (current_state);
 		}
-		if (indx > convs.Count - 1) {
-			//panel.SetActive (false);
-			//indx = 0;
-			//iSActivated = false;
-			throw_panel();
+
+	}
+	private void npc_next_dia(int cur_state){
+		switch (cur_state) {
+		case 0:
+			npc_next_dia0 ();
+			break;
+		case 1:
+			npc_next_dia1 ();
+			break;
+		case 2:
+			npc_next_dia2 ();
+			break;
 		}
 	}
 
+	private void npc_next_dia0(){
 
-	private void npc_next_dia(){
-		indx++;
-		//Debug.Log ("next_dia");
-		Debug.Log (convs[indx]);
 		if (indx <= convs.Count-1) {
 			txt.text = convs [indx];
 			name_txt.text = npc_name;
-			//indx++;
-			//Debug.Log (convs.Count);
+			indx++;
+
 		} else {
-			//Debug.Log("set done");
+
 			throw_panel();
 		}
-		//indx++;
+
+
+	}
+	private void npc_next_dia1(){
+
+		if (indx <= convs1.Count-1) {
+			txt.text = convs1 [indx];
+			name_txt.text = npc_name;
+			indx++;
+		} else {
+			current_state=2;
+			throw_panel();
+		}
+	
+	}
+	private void npc_next_dia2(){
+		if (indx <= convs2.Count-1) {
+			txt.text = convs2 [indx];
+			name_txt.text = npc_name;
+			indx++;
+		} else {
+			throw_panel();
+		}
 
 	}
 	public void OnTriggerEnter2D(Collider2D cl){
 		if (cl.gameObject.tag == "Player")
-		{		//throw_panel ();
+		{
 			iSActivated = true;
 
 			panel.SetActive (true);
 			indx = 0;
-			txt.text = convs [indx];
-			name_txt.text = npc_name;
+			if (mission_comp&&current_state!=2) {
+				current_state = 1;
+			}
+			npc_next_dia (current_state);
 		}
 	}
 	public void OnTriggerExit2D(Collider2D cl){
@@ -73,10 +101,6 @@ public class npc : MonoBehaviour {
 		panel.SetActive (false);
 		indx = 0;
 		iSActivated = false;
-
-		//Debug.Log("set done");
-		//name_txt.text = npc_name;
-		//txt.text = convs [indx];
-		//indx++;
 	}
+
 }
