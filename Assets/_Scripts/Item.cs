bb19d2f7a1c138dcfe9beba45fc3a06cc5f61da3
Ztz <ztz_1@qq.com>
2017-04-26 16:03:55 +0100

    item.cs

--- Assets/_Scripts/Item.cs
+++ Assets/_Scripts/Item.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour {
-	public Sprite icon;
-	public string item_name;
-	public int number;
+	//public Sprite icon;
+	//public string item_name;
+	//public int numb
 	private UIDController sc;
 	public InventoryItem the_item;
 	// Use this for initialization
 	void Start () {
 		GameObject item_script = GameObject.Find ("CanvasUID");
 		sc = item_script.GetComponent<UIDController> ();
-		the_item.icon = icon;
-		the_item.itemName = item_name;
-		the_item.itemCount = number;
+	//	the_item.icon = icon;
+	//	the_item.itemName = item_name;
+	//	the_item.itemCount = number;
 	}
 	
 	// Update is called once per frame
